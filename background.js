6b2b2812d2fd9fea6600279e827ef52904e97b83
lemonnuggets <63046776+lemonnuggets@users.noreply.github.com>
2021-04-30 20:16:31 +0530

    Added blocking & redirection functionality.
    When going to the hardcoded url, popup blocks video. Other videos will
    not be blocked.
    
    Added a few todos.
    
    Pages with videos in them will allow user to report the video in that
    page by clicking on the context menu. User will be alerted if no video
    exists on the page.

--- background.js
+++ background.js
@@ -1,1 +1,74 @@
 console.log("in background.js");
+// TODO: whitelisted urls
+// ############################## Blocker ##################################
+const BLOCKED_URLS = [
+    {
+        url: "https://www.youtube.com/watch?v=DXUAyRRkI6k",
+        tags: ["cats", "no dogs"],
+    },
+];
+
+const isBlockedURL = (url) => {
+    for (blockedURL of BLOCKED_URLS) {
+        console.log(blockedURL);
+        console.log(blockedURL.url, url, blockedURL.url === url);
+        if (blockedURL.url === url) return blockedURL;
+    }
+    return false;
+};
+
+// Listen for when a Tab changes state
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo && changeInfo.status == "complete") {
+        console.log("Tab updated: ", tabId, changeInfo, tab);
+        const blockedURL = isBlockedURL(tab.url);
+        console.log(`blocked url = ${blockedURL}`);
+
+        if (blockedURL) {
+            console.log("Blocking", tab.url);
+            chrome.tabs.sendMessage(tabId, {
+                blockContent: true,
+                redirectURL: `http://localhost:3000/home/?url=${tab.url}`,
+                contains: blockedURL.tags.join(", "),
+            });
+        } else {
+            console.log("Unblocking", tab.url);
+            chrome.tabs.sendMessage(tabId, { blockContent: false });
+        }
+    }
+});
+
+// ############################## Context Menu #############################
+let contextMenuItem = {
+    id: "reportRedirect",
+    title: "Report Video in Page",
+    contexts: ["all"],
+};
+chrome.contextMenus.create(contextMenuItem);
+chrome.contextMenus.onClicked.addListener((clickData) => {
+    console.log(clickData);
+    if (clickData.menuItemId === "reportRedirect") {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                {
+                    getInfo: true,
+                },
+                (response) => {
+                    if (response.videoElementPresent) {
+                        chrome.tabs.sendMessage(tabs[0].id, {
+                            redirect: true,
+                            redirectURL: `http://localhost:3000/home/?url=${clickData.pageUrl}`,
+                        });
+                    } else {
+                        chrome.tabs.sendMessage(tabs[0].id, {
+                            type: "alert",
+                            message:
+                                "Could not find any video element in the page.",
+                        });
+                    }
+                }
+            );
+        });
+    }
+});
--- blocker/blocker.js
+++ blocker/blocker.js
@@ -1,1 +1,111 @@
 console.log("in blocker.js");
+
+// const allLinks = document.querySelectorAll("a");
+// // const isThumbLink = (linkElement) => {
+// //     const classes = linkElement.classList
+// // }
+// allLinks.filter(isThumbLink);
+let tobeblocked = false;
+
+const blocker = document.createElement("div");
+blocker.className = "blocker";
+
+const tooltip = document.createElement("div");
+tooltip.className = "tooltip";
+const textContainer = document.createElement("div");
+textContainer.className = "text-container";
+textContainer.innerText =
+    "This video might contain unethical content. Click to find out more.";
+tooltip.appendChild(textContainer);
+const tooltiptext = document.createElement("span");
+tooltiptext.className = "tooltiptext";
+tooltip.appendChild(tooltiptext);
+blocker.appendChild(tooltip);
+console.log("IN BLOCKER.JS");
+
+const renderBlocker = (videoPlayer) => {
+    if (tobeblocked) {
+        const {
+            x,
+            y,
+            width,
+            height,
+            top,
+            right,
+            bottom,
+            left,
+        } = videoPlayer.getBoundingClientRect();
+        blocker.style.width = `${width + 5}px`;
+        blocker.style.height = `${height + 5}px`;
+        blocker.style.top = `${top}px`;
+        blocker.style.left = `${left}px`;
+        videoPlayer.pause();
+        console.log(videoPlayer.getBoundingClientRect());
+    }
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.blockContent === true) {
+        tobeblocked = true;
+        console.log("MASKED EXTENSION IS ACTIVE");
+        let videoPlayer;
+        videoPlayer = document.querySelector("video");
+        console.log(videoPlayer);
+        setInterval(() => {
+            if (tobeblocked) {
+                videoPlayer = document.querySelector("video");
+                console.log(videoPlayer);
+                videoPlayer.pause();
+            }
+        }, 5000);
+
+        if (request.redirectURL !== undefined) {
+            if (tobeblocked) {
+                blocker.onclick = () => {
+                    window.open(request.redirectURL, "_blank");
+                    tobeblocked = false;
+                    document.body.removeChild(blocker);
+                };
+            }
+        }
+        document.body.insertBefore(blocker, document.body.children[0]);
+        renderBlocker(videoPlayer);
+        window.onscroll = () => {
+            renderBlocker(videoPlayer);
+        };
+        window.onchange = () => {
+            renderBlocker(videoPlayer);
+        };
+        window.onresize = () => {
+            renderBlocker(videoPlayer);
+        };
+        videoPlayer.onplay = () => {
+            renderBlocker(videoPlayer);
+        };
+        if (request.contains !== undefined) {
+            if (tobeblocked) {
+                document.querySelector(
+                    ".tooltiptext"
+                ).innerText = `This video has been reported as it contains: ${request.contains}`;
+            }
+        }
+    } else if (request.blockContent === false) {
+        tobeblocked = false;
+        document.body.removeChild(blocker);
+    }
+    if (request.getInfo === true) {
+        console.log("getting info");
+        if (document.querySelector("video")) {
+            sendResponse({ videoElementPresent: true });
+        } else {
+            sendResponse({ videoElementPresent: false });
+        }
+        return true;
+    }
+    if (request.redirect === true) {
+        window.open(request.redirectURL, "_blank");
+    }
+    if (request.type === "alert") {
+        alert(request.message);
+    }
+});
